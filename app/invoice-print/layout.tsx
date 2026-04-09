74f93015eee1cb17f39e2ab9b1e11f5ce224534a
Cejo911 <105250660+Cejo911@users.noreply.github.com>
2026-04-09 22:59:03 +0100

    Delete app/invoice-print directory

--- app/invoice-print/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function PrintLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, fontFamily: 'Arial, sans-serif', background: '#fff' }}>
-        {children}
-      </body>
-    </html>
-  )
-}
